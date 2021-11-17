6b8c71e78652bed135042cea5b6937637b4b0135
Sherry Fan <sherryfansf@berkeley.edu>
2021-11-16 16:09:39 -0800

    added skeleton

---
+++ input-gen.py
@@ -0,0 +1,9 @@
+from random import randint, uniform
+
+n = int(input())
+for i in range(n):
+	task_id = i + 1
+	deadline = randint(1, 1440)
+	duration = randint(1, 60)
+	profit = round(uniform(0.001, 99.999), 3)
+	print("{0} {1} {2} {3}".format(task_id, deadline, duration, profit))
