ed548ffc3048a207ea5d86b73762ef077cb66817
Sherry Fan <sherryfansf@berkeley.edu>
2021-11-16 17:06:01 -0800

    added eval

--- greedy-most-profit.py
+++ greedy-most-profit.py
@@ -1,5 +1,6 @@
 from parse import read_input_file, write_output_file
 import os
+from igloovalue import eval_igloos
 
 def solve(tasks):
     """
@@ -19,12 +20,10 @@ def solve(tasks):
             newprofit = best.get_max_benefit()
             profit += newprofit
             tasks = list(filter(
-                lambda x: (x.get_late_benefit(end + x.get_duration() - x.get_deadline()) >= 0.5 * profit or x.get_deadline() > end)
-                and (x.get_task_id() != best.get_task_id()), tasks))
+                lambda x: (x.get_late_benefit(end + x.get_duration() - x.get_deadline()) >= 0.5 * newprofit or x.get_deadline() > end) and (x.get_task_id() != best.get_task_id()), tasks))
             igloos.append(best.get_task_id())
         else:
             break
-    print(igloos)
     return igloos
 
 if __name__ == '__main__':
@@ -33,3 +32,4 @@ if __name__ == '__main__':
         tasks = read_input_file('inputs/' + input_path)
         output = solve(tasks)
         write_output_file(output_path, output)
+        print(eval_igloos(tasks, output))
---
+++ igloovalue.py
@@ -0,0 +1,22 @@
+import Task
+
+def eval_igloos(tasks, task_ids):
+	"""Calculates value of a sequence of igloo indices.
+	IGLOOS - a list of igloos (Tasks)
+	INDICES - a list of indices of polished igloo
+	"""
+	total_val = 0
+	time = 0
+	for ind in task_ids:
+		next_igloo = tasks[ind - 1]
+		deadline = next_igloo.get_deadline()
+		if time <= deadline:
+			total_val += next_igloo.get_max_benefit()
+		else:
+			total_val += next_igloo.get_late_benefit(time - deadline)
+		time += next_igloo.get_duration()
+
+	total_profit = sum([task.get_max_benefit() for task in tasks])
+	total_time = sum([task.get_duration() for task in tasks])
+	upper_bound = total_profit * total_time / 1440
+	return total_val, upper_bound
